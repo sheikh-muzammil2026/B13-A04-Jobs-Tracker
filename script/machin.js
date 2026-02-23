9e51db03b8b86cfda1bcd02ff5877cbbab4b6bcc
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-02-23 22:19:52 +0600

    Update machin.js

--- script/machin.js
+++ script/machin.js
@@ -1,97 +1,126 @@
 
-
-  
+function updateAllCounts(){
+  // function to calculate total and available jobs
   let jobCardLen = document.querySelectorAll(".job-card");
 document.getElementById("available-count").innerText = jobCardLen.length;
-document.getElementById("job-count").innerText = jobCardLen.length;
+document.getElementById("total-job-count").innerText = jobCardLen.length;
 
+}
 
+updateAllCounts();
 
-// interview
+// when all jobs tab clicked, available jobs count will update.
 
-document.getElementById("btn-interview").addEventListener('click', function () {
-
-  // set badge 
-  // let badge = document.getElementById("badge");
-  // badge.classList.add("badge");
-  // badge.innerText ="Interview";
-
-  // interview count ++
-
- let count = parseInt(document.getElementById("interview-count").innerText);
-  
-  document.getElementById("interview-count").innerText = count + 1;
-
-  // copy to interview tab div
-
-  // let jobCard = document.getElementById("card-1");
- //  let clone = jobCard.cloneNode(true);
- // document.getElementById("tab-content").appendChild(clone);
-  
- //  document.getElementById("interview-tab").remove();
- 
+document.addEventListener('click', function (e){
+  if(e.target.closest(".all-jobs-tab")){
+  let cards =  document.querySelectorAll("#all-tab-content .job-card");
+    document.getElementById("available-count").innerText = cards.length ;
+  }
 });
 
 
-// btn delete 
-// document.querySelector(".btn-delete").addEventListener('click', function () {
+// when interview tab clicked, available jobs count will update.
+document.addEventListener('click', function (e){
+  if(e.target.closest(".interview-tab")){
+  let cards =  document.querySelectorAll("#interview-tab-content .job-card");
+    document.getElementById("available-count").innerText = `${cards.length} of 8`;
+  }
+});
 
-//  let btnParent = this.parentElement;
-//   btnParent.remove();
+// when rejected  tab clicked, available jobs count will update.
+document.addEventListener('click', function (e){
+  if(e.target.closest(".rejected-tab")){
+  let cards =  document.querySelectorAll("#rejected-tab-content .job-card");
+    document.getElementById("available-count").innerText = `${cards.length} of 8`;
+  }
+});
 
 
-// });
+
+//  function to calculate tabs child
+function updateCounts(tabId,countId){
+  let jobCard = document.querySelectorAll(`#${tabId} .job-card`);
+  document.getElementById(countId).innerText = jobCard.length;
+  
+}
+
+
 
 // multiple parent element  delete with btn-delete
+
 document.addEventListener('click', function (e) {
 let btn = e.target.closest(".btn-delete");
   if(btn){
     btn.parentElement.remove();
+    updateAllCounts();
   }
 });
 
 
-// multiple interview 
+
 
 document.addEventListener('click', function (e) {
-let btn = e.target.closest(".btn-interview");
+ let btn = e.target.closest(".btn-interview");
   if(btn){
-  let clone =  btn.parentElement.cloneNode(true);
-    document.getElementById("interview-tab-content").appendChild(clone);
-  
-  document.getElementById("interview-empty-content").remove();
+  let clone =  btn.parentElement.parentElement.cloneNode(true); // copied
 
-        // set badge 
-  // let badge = e.target.closest(".badge");
-    let badge = document.querySelector(".badge");
-    newBadge = document.createElement("button");
-  newBadge.classList.add("badge-style");
-  newBadge.innerText ="Interview";
-    badge.replaceWith(newBadge);
-  
+    // set badge 
+  let cloneBadge = clone.querySelector(".badge");
+  let orginalbadge = btn.closest(".job-card").querySelector(".badge");
+    
+
+  cloneBadge.classList.add("badge-interview");
+  cloneBadge.innerText ="Interview";
+
+
+  orginalbadge.classList.add("badge-interview");
+  orginalbadge.innerText ="Interview";
+    
+ 
+    
+  document.getElementById("interview-tab-content").appendChild(clone); // pasted
+  let empty = document.getElementById("interview-empty-content"); // old content deleted
+if(empty){
+  empty.remove();
+}
+  updateCounts("interview-tab-content","interview-count")
+    
   }
 });
 
 
-// rejected tab 
+// copy to rejected tab 
 
 document.addEventListener('click', function (e) {
 let btn = e.target.closest(".btn-rejected");
   if(btn){
-  let clone =  btn.parentElement.cloneNode(true);
+  let clone =  btn.parentElement.parentElement.cloneNode(true);
+
+// set badge 
+    
+   let orginalBadge2 = btn.closest(".job-card").querySelector(".badge");
+   let cloneBadge2 = clone.querySelector(".badge");
+
+  orginalBadge2.classList.add("badge-error");
+  orginalBadge2.innerText ="Rejected";
+  
+  cloneBadge2.classList.add("badge-error");
+  cloneBadge2.innerText ="Rejected";
+
+
+    
     document.getElementById("rejected-tab-content").appendChild(clone);
   
-  document.getElementById("rejected-empty-content").remove();
+ let empty2 = document.getElementById("rejected-empty-content");
+    if(empty2){
+      empty2.remove();
+    }
 
-    // set badge 
-  // let badge = e.target.closest(".badge");
-    let badge = document.querySelector(".badge");
-    newBadge = document.createElement("button");
-  newBadge.classList.add("badge-style");
-  newBadge.innerText ="Rejected";
-    badge.replaceWith(newBadge);
+    updateCounts("rejected-tab-content", "rejected-count")
+    
   }
 });
+
 
 
 
